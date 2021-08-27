2205cddae9345947c6e6610fba51972dc5064849
Cadu Thompson <carlosethompson@gmail.com>
2021-08-27 11:40:50 +0100

    section-02-lecture-20: declaring and defining variables

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,16 @@
-alert('This works!');
+/* Variable Naming
+  ---------------------
+   #Allowed:
+   let userName       =   Best Practice: camelCase
+   let ageGroup5      =   Only letters and digits
+   let $kindOfSpecial =   Starting with $ is allowed
+   let _internalValue =   Starting with _ is allowed
+   
+   Not Allowed:
+   let user_name      =   Allowed but bad practice!
+   let 21Players      =   Starting digits
+   let user-b         =   No special characters!
+   let let            =   Keywords not allowed 
+*/
+
+let currentResult = 0;
